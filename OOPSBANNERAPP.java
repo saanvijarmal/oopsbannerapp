7f74829b1ab85f0614b5ce9a92ad340e2ddde4ed
saanvijarmal <itssaanvirj@gmail.com>
2026-02-19 10:31:07 +0530

    Add files via upload

---
+++ OOPSBANNERAPP.java
@@ -0,0 +1,11 @@
+public class OOPSBannerApp {
+
+    /**
+     * The main method serves as the entry point of the application.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
