de460a4c6dc987639ea2f74afad996d6d20e6f01
saanvijarmal <itssaanvirj@gmail.com>
2026-03-06 10:44:07 +0530

    feature/uc3

---
+++ src/OOPSBannerUC3.java
@@ -0,0 +1,53 @@
+class OopsBannerApp {
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ",
+                "  *****  ",
+                "  *****  ",
+                "  ****** ",
+                "  ****** "
+        ));
+
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *      "
+        ));
+
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *      "
+        ));
+
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " ******  ",
+                " *****  "
+        ));
+
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *       ",
+                "     *  "
+        ));
+
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *       ",
+                "     *  "
+        ));
+
+        System.out.println(String.join(" ",
+                "  *****  ",
+                "  *****  ",
+                " *       ",
+                " ****** "
+        ));
+    }
+}
